d67c6540ef3f2f3d4255f7ba75eec533a5e1f60e
bigmasy <jura.tymosh@gmail.com>
2025-08-16 13:03:56 +0300

    hw

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-axios.defaults.headers.common.Authorization = `Client-ID ${
-  import.meta.env.VITE_UNSPLASH_ACCESS_KEY
-}`;
+axios.defaults.headers.common.Authorization = `Client-ID vPGicYQfmcCL2RmGWhY-0YiOcYvbDAKvPZ70mt48bjU`;
 axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const fetchPhotos = async (query, page, per_page) => {
